f583cc76f157e0f01c9a5d354dc0b5f4ed5fb3f7
Enai63 <Toha76@yandex.ru>
2022-02-16 11:11:00 +0400

    feat: add entity Airplane

---
+++ src/main/java/com/mcb/creditfactory/model/Airplane.java
@@ -0,0 +1,54 @@
+package com.mcb.creditfactory.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.RequiredArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import org.hibernate.Hibernate;
+import javax.persistence.Id;
+import javax.persistence.Entity;
+import javax.persistence.Column;
+import javax.persistence.Table;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import java.util.Objects;
+
+
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "AIRPLANE")
+public class Airplane {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String brand;
+    private String model;
+    private String manufacturer;
+
+    @Column(name = "year_of_issue")
+    private Short year;
+
+    private Integer fuelCapacity;
+
+    private Integer seats;
+
+    @Override
+    public boolean equals(Object a) {
+        if (this == a) return true;
+        if (a == null || Hibernate.getClass(this) != Hibernate.getClass(a)) return false;
+        Airplane airplane = (Airplane) a;
+        return id != null && Objects.equals(id, airplane.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
